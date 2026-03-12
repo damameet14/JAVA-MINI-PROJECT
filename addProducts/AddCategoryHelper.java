08c3e59f7c4a0327c9aa618129cb329637b5e907
DarshPatadiya <darshpatadiya07@gmail.com>
2026-03-12 14:36:47 +0530

    darsh changes

---
+++ addProducts/AddCategoryHelper.java
@@ -0,0 +1,45 @@
+package addProducts;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class AddCategoryHelper {
+    public static void CategoryEntry() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Product Category Name: ");
+        String PRODUCT_CATEGORY_NAME = sc.nextLine();
+        String url = "jdbc:ucanaccess://JAVA_DATABASE.mdb";
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection conn = DriverManager.getConnection(url);
+            String sql = "INSERT INTO PRODUCT_CATEGORY (PRODUCT_CATEGORY_NAME) VALUES (?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, PRODUCT_CATEGORY_NAME);
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Add Category Helper placeholder");
+    }
+
+    public static void display() {
+
+        String url = "jdbc:ucanaccess://JAVA_DATABASE.mdb";
+        try {
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            Connection conn = DriverManager.getConnection(url);
+            String sql = "SELECT * FROM PRODUCT_CATEGORY";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                System.out.print("Name ");
+                System.out.println(rs.getString("PRODUCT_CATEGORY_NAME"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
